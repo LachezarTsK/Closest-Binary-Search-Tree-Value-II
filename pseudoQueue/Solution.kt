9f6fce28b66714d1d77b87725f70d4f0e8a20ddf
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-08-02 00:23:18 +0200

    Create Solution.kt

---
+++ pseudoQueue/Solution.kt
@@ -0,0 +1,34 @@
+
+import kotlin.math.abs
+
+class Solution {
+
+    private var front = 0
+    private val queue = mutableListOf<Int>()
+
+    fun closestKValues(root: TreeNode?, targetValue: Double, targetNumberOfNodes: Int): List<Int> {
+        inOrderSearch(root, targetValue, targetNumberOfNodes)
+        return queue.subList(front, queue.size)
+    }
+
+    private fun inOrderSearch(root: TreeNode?, targetValue: Double, targetNumberOfNodes: Int) {
+        if (root == null) {
+            return
+        }
+        inOrderSearch(root.left, targetValue, targetNumberOfNodes)
+        updateQueue(root, targetValue, targetNumberOfNodes)
+        inOrderSearch(root.right, targetValue, targetNumberOfNodes)
+    }
+
+    private fun updateQueue(root: TreeNode, targetValue: Double, targetNumberOfNodes: Int) {
+        queue.add(root.`val`)
+        if (queue.size <= targetNumberOfNodes) {
+            return
+        }
+        if (abs(queue[front] - targetValue) > abs(queue.last() - targetValue)) {
+            ++front
+            return
+        }
+        queue.removeLast()
+    }
+}
