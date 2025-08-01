9e51f95735f55227ca22547c031ec40a5c935ae7
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-08-02 00:18:41 +0200

    Create Solution.kt

---
+++ deque/Solution.kt
@@ -0,0 +1,33 @@
+
+import kotlin.math.abs
+import kotlin.collections.ArrayDeque
+
+class Solution {
+
+    fun closestKValues(root: TreeNode?, targetValue: Double, targetNumberOfNodes: Int): List<Int> {
+        val queue = ArrayDeque<Int>()
+        inOrderSearch(root, targetValue, targetNumberOfNodes, queue)
+        return ArrayList<Int>(queue)
+    }
+
+    private fun inOrderSearch(root: TreeNode?, targetValue: Double, targetNumberOfNodes: Int, queue: ArrayDeque<Int>) {
+        if (root == null) {
+            return
+        }
+        inOrderSearch(root.left, targetValue, targetNumberOfNodes, queue)
+        updateQueue(root, targetValue, targetNumberOfNodes, queue)
+        inOrderSearch(root.right, targetValue, targetNumberOfNodes, queue)
+    }
+
+    private fun updateQueue(root: TreeNode, targetValue: Double, targetNumberOfNodes: Int, queue: ArrayDeque<Int>) {
+        queue.add(root.`val`)
+        if (queue.size <= targetNumberOfNodes) {
+            return
+        }
+        if (abs(queue.first() - targetValue) > abs(queue.last() - targetValue)) {
+            queue.removeFirst()
+            return
+        }
+        queue.removeLast()
+    }
+}
